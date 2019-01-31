bee905f32c726d7dc5a7e698998bd62c55c8ed7a
Maskoid Technologies <46883634+maskoid@users.noreply.github.com>
2019-01-31 16:11:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 yum -y update
 yum -y install wget
 yum -y install nano
-cd /etc/yum.repos.d && curl https://raw.githubusercontent.com/maskoid/vestacustom/master/yum.repos.d/MariaDB.repo?token=AstjMgQjG5lqFwlm7pzeTzdiFqnDcO-Gks5cUsXuwA%3D%3D -o MariaDB.repo
-cd /etc/yum.repos.d && curl https://raw.githubusercontent.com/maskoid/vestacustom/master/yum.repos.d/nginx.repo?token=AstjMhETIGKTVv0wH8XLMp5na0s0VZD_ks5cUsaMwA%3D%3D -o nginx.repo
-cd /etc/yum.repos.d && curl https://raw.githubusercontent.com/maskoid/vestacustom/master/yum.repos.d/vesta.repo?token=AstjMqod2QsNI1sxw56GyexawR0-9qhZks5cUsa8wA%3D%3D -o vesta.repo
+cd /etc/yum.repos.d && curl https://raw.githubusercontent.com/maskoid/vestacustom/master/yum.repos.d/MariaDB.repo -o MariaDB.repo
+cd /etc/yum.repos.d && curl https://raw.githubusercontent.com/maskoid/vestacustom/master/yum.repos.d/nginx.repo -o nginx.repo
+cd /etc/yum.repos.d && curl https://raw.githubusercontent.com/maskoid/vestacustom/master/yum.repos.d/vesta.repo -o vesta.repo
 wget c.vestacp.com/GPG.txt -O /etc/pki/rpm-gpg/RPM-GPG-KEY-VESTA
 cd /root/
 yum -y update
@@ -18,4 +18,4 @@ sudo yum-config-manager --enable remi-php73
 sudo yum-config-manager --enable remi
 yum -y install php-cli php-imap php-process php-pspell php-xml php-xmlrpc php-pdo php-ldap php-pecl-zip php-common php-mcrypt php-mysqlnd php-gmp php-mbstring php-gd php-tidy php-pecl-memcache php-opcache --enablerepo=remi
 yum -y install php56-php-cli php56-php-imap php56-php-process php56-php-pspell php56-php-xml php56-php-xmlrpc php56-php-pdo php56-php-ldap php56-php-pecl-zip php56-php-common php56-php-mcrypt php56-php-mysqlnd php56-php-gmp php56-php-mbstring php56-php-gd php56-php-tidy php56-php-pecl-memcache php56-php-opcache --enablerepo=remi
-curl https://raw.githubusercontent.com/maskoid/vestacustom/master/vst-install.sh?token=AstjMs8CAvJ8oIzEf5DTJwxU-jdY2PWKks5cUseXwA%3D%3D -o vst-install.sh
+curl https://raw.githubusercontent.com/maskoid/vestacustom/master/vst-install.sh -o vst-install.sh
