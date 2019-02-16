c6780e0d61b7887a811c7210506f7fc30a6723c4
Maskoid Technologies <46883634+maskoid@users.noreply.github.com>
2019-02-16 10:43:32 +0530

    Update dhclient.sh

--- scripts/dhclient.sh
+++ scripts/dhclient.sh
@@ -2,7 +2,7 @@
 # Solution for "No Internet Connection from VMware"
 #
 cd /etc/init.d
-wget https://raw.githubusercontent.com/maskoid/vestacustom/master/scripts/net-autostart
+curl -O https://raw.githubusercontent.com/maskoid/vestacustom/master/scripts/net-autostart
 chmod 755 net-autostart
 chkconfig --add net-autostart
 echo "rebooting in 5 seconds"
