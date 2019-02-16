7b8b8c11ea4ff80a1abbc75e738748f171bfcb81
Maskoid Technologies <46883634+maskoid@users.noreply.github.com>
2019-02-16 10:44:33 +0530

    Update dhclient.sh

--- scripts/dhclient.sh
+++ scripts/dhclient.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Solution for "No Internet Connection from VMware"
 #
+dhclient –v
+echo "dhclient enabled. Now add this in auto-startup "
 cd /etc/init.d
 curl -O https://raw.githubusercontent.com/maskoid/vestacustom/master/scripts/net-autostart
 chmod 755 net-autostart
