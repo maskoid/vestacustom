91b5efa816b3e9f14c5552f71f64f273e4b8b3e7
Maskoid Technologies <46883634+maskoid@users.noreply.github.com>
2019-02-26 18:13:13 +0530

    Update vst-install-rhel.sh

--- vst-install-rhel.sh
+++ vst-install-rhel.sh
@@ -24,7 +24,6 @@ yum-config-manager --disable remi-php54
 sudo yum-config-manager --enable remi-php73
 sudo yum-config-manager --enable remi
 yum -y install php-cli php-imap php-process php-pspell php-xml php-xmlrpc php-pdo php-ldap php-pecl-zip php-common php-mcrypt php-mysqlnd php-gmp php-mbstring php-gd php-tidy php-pecl-memcache php-opcache --enablerepo=remi
-yum -y install php56-php-cli php56-php-imap php56-php-process php56-php-pspell php56-php-xml php56-php-xmlrpc php56-php-pdo php56-php-ldap php56-php-pecl-zip php56-php-common php56-php-mcrypt php56-php-mysqlnd php56-php-gmp php56-php-mbstring php56-php-gd php56-php-tidy php56-php-pecl-memcache php56-php-opcache --enablerepo=remi
 
 #php 7.3 and 5.6 install, system updated and latest repo downloaded and activated
 
