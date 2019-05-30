cd2c125825ede77060b6ef22c48a796ce15e5719
Maskoid Technologies <46883634+maskoid@users.noreply.github.com>
2019-05-30 11:52:34 +0530

    Create backup.sh

---
+++ backup/backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# VestaCP Dropbox Backup Setup
+# Create App: https://www.dropbox.com/developers/apps and get access token
+cd /
+mkdir dropbox
+cd dropbox
+curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
+chmod +x dropbox_uploader.sh
+curl "https://raw.githubusercontent.com/maskoid/vestacustom/master/backup/send_site_backup_files_to_dropbox.sh" -o /usr/local/vesta/bin/send_site_backup_files_to_dropbox.sh
+chmod 755 /usr/local/vesta/bin/send_site_backup_files_to_dropbox.sh
