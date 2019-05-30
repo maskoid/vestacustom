307cffbc40f1f5ebc6b4be9fc56cb7b2355662dc
Maskoid Technologies <46883634+maskoid@users.noreply.github.com>
2019-05-30 12:20:20 +0530

    Update backupallusers.sh

--- backup/backupallusers.sh
+++ backup/backupallusers.sh
@@ -10,14 +10,11 @@
 #                    Variable&Function                     #
 #----------------------------------------------------------#
 
-# Argument definition
-format=${1-shell}
-
 # Includes
 source $VESTA/func/main.sh
 
 # SHELL list function
-shell_list() {
+backup_all_users() {
     echo "USER"
     echo "----"
     while read user; do
@@ -26,26 +23,13 @@ shell_list() {
     done < <(grep @ /etc/passwd |cut -f 1 -d :)
 }
 
-# PLAIN list function
-plain_list() {
-    while read user; do
-        echo "$user"
-        echo "plain" 
-    done < <(grep @ /etc/passwd |cut -f 1 -d :)
-}
 
 
 #----------------------------------------------------------#
 #                       Action                             #
 #----------------------------------------------------------#
 
-# Listing data
-case $format in
-    json)   json_list ;;
-    plain)  plain_list ;;
-    csv)    csv_list ;;
-    shell)  shell_list ;;
-esac
+backup_all_users;
 
 
 #----------------------------------------------------------#
