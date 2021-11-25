b1e81533eb852cb657fb535f61ec9b9276cc5b38
Shubham Trivedi <62797077+MTPL-shubham@users.noreply.github.com>
2021-11-25 18:07:17 +0530

    Update backupallusers.sh

--- migration/backupallusers.sh
+++ migration/backupallusers.sh
@@ -17,14 +17,14 @@ dropbox=${1-shell}
 removeall=${2-shell}
 
 # Includes
-source $VESTA/func/main.sh
+source $HESTIA/func/main.sh
 
 # Backup All Users function
 backup_all_users() {
     echo "Backing Up All Users"
     echo "----"
     while read user; do
-        /usr/local/vesta/bin/v-backup-user $user
+        /usr/local/hestia/bin/v-backup-user $user
 
     done < <(grep @ /etc/passwd |cut -f 1 -d :)
 }
@@ -42,7 +42,7 @@ fi
 backup_all_users;
 
 if [ "$dropbox" = "yes" ]; then
-        sudo /usr/local/vesta/bin/send_site_backup_files_to_dropbox.sh
+        sudo /usr/local/hestia/bin/send_site_backup_files_to_dropbox.sh
 fi
 
 #----------------------------------------------------------#
